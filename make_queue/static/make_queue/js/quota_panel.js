ddc86f1f635e90c67f0dafc1cc7a842834f7b2ef
Herman Berget <32880010+hermabe@users.noreply.github.com>
2018-11-23 22:28:41 +0100

    Added chechbox to hide used quotas in quota panel

--- make_queue/static/make_queue/js/quota_panel.js
+++ make_queue/static/make_queue/js/quota_panel.js
@@ -1,10 +1,19 @@
 $('#user').parent().dropdown({
-    onChange: function(username, text) {
+    onChange: function (username, text) {
         $.ajax(langPrefix + "/reservation/quota/user/" + username + "/", {
             success: function (data, textStatus) {
                 $("#user-quotas").html(data);
                 setupDeleteModal();
             }
         })
+    }
+});
+
+$("#hide_used_quotas").checkbox({
+    onChange: function () {
+        $(".card").filter(function () {
+            console.log($(this).data("reservations-left"))
+            return !($(this).data("is-diminishing") === "True") || parseInt($(this).data("reservations-left")) <= 0;
+        }).toggleClass("make_hidden", $(this).is(":checked"))
     }
 });
